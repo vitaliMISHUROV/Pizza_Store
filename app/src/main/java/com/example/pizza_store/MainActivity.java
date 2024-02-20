ca3bf9156d2afe5cbf1da3e3e9b888cc5be9fd30
vitali <vitalamisurov@gmail.com>
2024-02-20 02:41:14 +0200

    Add Result Activity

--- app/src/main/java/com/example/pizza_store/MainActivity.java
+++ app/src/main/java/com/example/pizza_store/MainActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
-
+//git
 public class MainActivity extends AppCompatActivity {
 
     public static final String KEYORDER = "keyorder";
